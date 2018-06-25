88c68264a8c6b6399758486797c995966202b1e3
simpart <simpart>
2018-06-26 05:58:26 +0900

    add python

---
+++ src/py/display.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+import serial
+import time
+
+if __name__ == "__main__":
+    try:
+	print(sys.argv[1])
+        #print("start")
+        ser = serial.Serial('/dev/ttyS0', 115200, timeout=60)
+        
+        bmp = open(sys.argv[1], 'rb')
+        dat = bmp.read()
+        ser.write(dat)
+        line = ser.readline()
+        #print(line)
+        
+        ser.close()
+        
+        #print("end")
+    except:
+        import traceback
+        traceback.print_exc()
+
